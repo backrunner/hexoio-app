c2f7f9b142dabb77447d57c8150bce49914446ea
BackRunner <liaoyp126@126.com>
2018-08-18 16:40:23 +0800

    finish search logic

--- app/src/main/java/top/backrunner/hexoio/MainActivity.java
+++ app/src/main/java/top/backrunner/hexoio/MainActivity.java
@@ -5,8 +5,12 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.icu.text.LocaleDisplayNames;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
@@ -22,6 +26,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
@@ -30,11 +35,20 @@ import android.webkit.WebView;
 import android.util.Log;
 import android.webkit.WebViewClient;
 
+import java.io.ByteArrayInputStream;
+import java.lang.ref.WeakReference;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    //views define
     private WebView mWebView;
+    private NavigationView mNaviView;
+
+    //set up handler
+    private static Handler navHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,11 +62,14 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        mNaviView = (NavigationView) findViewById(R.id.nav_view);
+        mNaviView.setNavigationItemSelectedListener(this);
 
         //set WebView
         mWebView = findViewById(R.id.mWebView);
+
+        //add js interface
+        mWebView.addJavascriptInterface(new navStatus(), "navStatus");
 
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -61,19 +78,32 @@ public class MainActivity extends AppCompatActivity
         webSettings.setAllowContentAccess(true);
         webSettings.setUserAgentString("app/hexoio");
         mWebView.clearCache(false);
-        mWebView.loadUrl("https://io.backrunner.top/");
+        mWebView.loadUrl("https://io.backrunner.top");
+
+        mNaviView.setCheckedItem(R.id.nav_home);
+
         mWebView.setWebViewClient(new WebViewClient(){
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 currentUrl = url;
+
+                if (currentUrl.contains("categories")) {
+                    mNaviView.setCheckedItem(R.id.nav_categories);
+                    currentSelectedNavItem = R.id.nav_categories;
+                } else if(currentUrl.contains("tags")){
+                    mNaviView.setCheckedItem(R.id.nav_tag);
+                    currentSelectedNavItem = R.id.nav_tag;
+                } else {
+                    mNaviView.setCheckedItem(R.id.nav_home);
+                    currentSelectedNavItem = R.id.nav_home;
+                }
+
                 super.onPageStarted(view, url, favicon);
             }
 
             @Override
             public void onPageFinished(WebView view, String url) {
-                if (!url.contains("page")){
-                    mWebView.scrollTo(0,0);
-                }
+                mWebView.scrollTo(0,0);
                 super.onPageFinished(view, url);
             }
 
@@ -89,22 +119,61 @@ public class MainActivity extends AppCompatActivity
                     return super.shouldOverrideUrlLoading(view, request);
                 }
             }
+
+            @Nullable
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                //refuse to load changyan script
+                if (request.getUrl().toString().contains("changyan")){
+                    String noresponse = "";
+                    WebResourceResponse intercepted = new WebResourceResponse("text/javascript","utf8", new ByteArrayInputStream(noresponse.getBytes()));
+                    return intercepted;
+                } else {
+                    return super.shouldInterceptRequest(view, request);
+                }
+            }
+
         });
     }
 
-    private String currentUrl = "";
+    @Override
+    protected void onDestroy() {
+        navHandler.removeCallbacksAndMessages(null);
+        super.onDestroy();
+    }
 
+    private String currentUrl = "";
+    private int currentSelectedNavItem;
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
+            Log.d("Current URL",currentUrl);
+            Log.d("Bool",Boolean.toString(currentUrl == "https://io.backrunner.top/" || currentUrl == "https://io.backrunner.top"));
+            Log.d("NavItem",Integer.toString(currentSelectedNavItem));
             //Drawer is off
-            if (currentUrl.contains("page")){
-                mWebView.goBack();
+            if (currentSelectedNavItem == R.id.nav_search){
+                mWebView.loadUrl("javascript:onPopupClose();");
+                if (currentUrl.contains("categories")) {
+                    mNaviView.setCheckedItem(R.id.nav_categories);
+                    currentSelectedNavItem = R.id.nav_categories;
+                } else if(currentUrl.contains("tags")){
+                    mNaviView.setCheckedItem(R.id.nav_tag);
+                    currentSelectedNavItem = R.id.nav_tag;
+                } else {
+                    mNaviView.setCheckedItem(R.id.nav_home);
+                    currentSelectedNavItem = R.id.nav_home;
+                }
             } else {
-                super.onBackPressed();
+                if (currentUrl.equals("https://io.backrunner.top/") || currentUrl.equals("https://io.backrunner.top")) {
+                    Log.d("Recycle","Back Pressed");
+                    navHandler.removeCallbacksAndMessages(null);
+                    super.onBackPressed();
+                } else {
+                    mWebView.goBack();
+                }
             }
         }
     }
@@ -114,15 +183,15 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        currentSelectedNavItem = id;
         if (id == R.id.nav_home) {
-            // Handle the camera action
+            mWebView.loadUrl("https://io.backrunner.top");
         } else if (id == R.id.nav_tag) {
-
+            mWebView.loadUrl("https://io.backrunner.top/tags");
         } else if (id == R.id.nav_categories) {
-
+            mWebView.loadUrl("https://io.backrunner.top/categories");
         } else if (id == R.id.nav_search) {
-
+            mWebView.loadUrl("javascript:searchTrigger();");
         } else if (id == R.id.nav_settings) {
 
         } else if (id == R.id.nav_exit) {
@@ -144,5 +213,26 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public final class navStatus{
+        @JavascriptInterface
+        public void restoreNavStatus(){
+            navHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (currentUrl.contains("categories")) {
+                        mNaviView.setCheckedItem(R.id.nav_categories);
+                        currentSelectedNavItem = R.id.nav_categories;
+                    } else if(currentUrl.contains("tags")){
+                        mNaviView.setCheckedItem(R.id.nav_tag);
+                        currentSelectedNavItem = R.id.nav_tag;
+                    } else {
+                        mNaviView.setCheckedItem(R.id.nav_home);
+                        currentSelectedNavItem = R.id.nav_home;
+                    }
+                }
+            });
+        }
     }
 }
